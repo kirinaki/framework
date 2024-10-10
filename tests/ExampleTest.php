eb2ea4004a2a74850c67c4f3802b067ac12584d7
Christopher Trevilla Figueroa <christophertrevilla@gmail.com>
2024-10-09 23:52:43 -0600

    installed phpunit

---
+++ tests/ExampleTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+
+    public function test_it_should_works()
+    {
+        $this->assertTrue(2 + 2 == 4);
+    }
+}
