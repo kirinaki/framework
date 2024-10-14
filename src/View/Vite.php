afdad6a83de613ce8f245caeaa3ba2532948e2ef
Christopher Trevilla Figueroa <christophertrevilla@gmail.com>
2024-10-14 01:35:20 -0600

    Update Vite.php

--- src/View/Vite.php
+++ src/View/Vite.php
@@ -42,39 +42,64 @@ class Vite
 
     public function register()
     {
-
         Wordpress::action("wp_enqueue_scripts", function () {
             $config = App::get("config");
             $manifestPath = $config["publicPath"] . $this->manifestPath;
             $fullUrl = "{$this->host}:{$this->port}";
 
-            if (is_array(Wordpress::remoteGet($fullUrl))) {
-                Wordpress::enqueueScript("vite", $fullUrl . "/@vite/client", [], null);
-                foreach ($this->entrypoints as $key => $entrypoint) {
-                    if (str_ends_with($entrypoint, ".css") || str_ends_with($entrypoint, ".scss") || str_ends_with($entrypoint, ".sass")) {
-                        Wordpress::enqueueStyle("module-$key", $fullUrl . $entrypoint, [], 'null');
-                    } else {
-                        Wordpress::enqueueStyle("module-$key", $fullUrl . $entrypoint, ['jquery'], null, true);
-                    }
-                }
-
+            if ($this->isRemoteAvailable($fullUrl)) {
+                $this->enqueueViteAssets($fullUrl);
             } elseif (Storage::exists($manifestPath)) {
-                $manifest = json_decode(Storage::get($manifestPath), true);
-                foreach ($this->entrypoints as $key => $entrypoint) {
-                    if (str_ends_with($entrypoint, ".css") || str_ends_with($entrypoint, ".scss") || str_ends_with($entrypoint, ".sass")) {
-                        Wordpress::enqueueStyle("module-$key", Wordpress::getThemeFileUri("public/dist/{$manifest[substr($entrypoint,1)]['file']}"), [], null);
-                    } else {
-                        Wordpress::enqueueScript("module-$key", Wordpress::getThemeFileUri("public/dist/{$manifest[substr($entrypoint,1)]['file']}"), ['jquery'], null, true);
-                    }
-                }
+                $this->enqueueLocalAssets($manifestPath);
             }
         });
 
         Wordpress::filter('script_loader_tag', function (string $tag, string $handle, string $src) {
-            if (in_array($handle, ['vite', 'main-js'])) {
-                return '<script type="module" src="' . Wordpress::escapeUrl($src) . '" defer></script>';
-            }
-            return $tag;
+            return $this->customScriptTag($tag, $handle, $src);
         }, 10, 3);
+    }
+
+    private function isRemoteAvailable(string $fullUrl): bool
+    {
+        return is_array(Wordpress::remoteGet($fullUrl));
+    }
+
+    private function enqueueViteAssets(string $fullUrl): void
+    {
+        Wordpress::enqueueScript("vite", $fullUrl . "/@vite/client", [], null);
+        foreach ($this->entrypoints as $key => $entrypoint) {
+            $this->enqueueAsset($key, $entrypoint, $fullUrl);
+        }
+    }
+
+    private function enqueueLocalAssets(string $manifestPath): void
+    {
+        $manifest = json_decode(Storage::get($manifestPath), true);
+        foreach ($this->entrypoints as $key => $entrypoint) {
+            $filePath = "public/dist/{$manifest[substr($entrypoint, 1)]['file']}";
+            $this->enqueueAsset($key, $entrypoint, Wordpress::getThemeFileUri($filePath));
+        }
+    }
+
+    private function enqueueAsset(string $key, string $entrypoint, string $url): void
+    {
+        if ($this->isStyleFile($entrypoint)) {
+            Wordpress::enqueueStyle("module-$key", $url . $entrypoint, [], null);
+        } else {
+            Wordpress::enqueueScript("module-$key", $url . $entrypoint, ['jquery'], null, true);
+        }
+    }
+
+    private function isStyleFile(string $entrypoint): bool
+    {
+        return str_ends_with($entrypoint, ".css") || str_ends_with($entrypoint, ".scss") || str_ends_with($entrypoint, ".sass");
+    }
+
+    private function customScriptTag(string $tag, string $handle, string $src): string
+    {
+        if (in_array($handle, ['vite', 'main-js'])) {
+            return '<script type="module" src="' . Wordpress::escapeUrl($src) . '" defer></script>';
+        }
+        return $tag;
     }
 }
