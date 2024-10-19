9755645519b6c5cebdc57757e44eb7be72d2776c
Christopher Trevilla Figueroa <christophertrevilla@gmail.com>
2024-10-19 16:41:57 -0600

    refactored tests

--- tests/ApplicationTest.php
+++ tests/ApplicationTest.php
@@ -3,15 +3,19 @@
 namespace Tests;
 
 use Kirinaki\Framework\Application\Application;
+use Kirinaki\Framework\Application\Configuration\ApplicationBuilder;
 use Kirinaki\Framework\Application\Configuration\ApplicationConfig;
 use Kirinaki\Framework\Discovery\Configuration\DiscoveryConfig;
 use Kirinaki\Framework\View\Configuration\ViewConfig;
+use Kirinaki\Framework\Vite\Configuration\ViteConfig;
+use Kirinaki\Framework\Vite\Vite;
+use Mockery;
 use phpmock\mockery\PHPMockery;
 use PHPUnit\Framework\TestCase;
 use Tests\Discovery\Stubs\DiscoverableAsActionStub;
 use Tests\Discovery\Stubs\DiscoverableAsPostTypeStub;
 use Tests\Discovery\Stubs\DiscoverableAsRestControllerStub;
-use Tests\Stubs\Functions\ExampleFunction;
+use Tests\Views\Stubs\Functions\ExampleFunction;
 
 class ApplicationTest extends TestCase
 {
@@ -23,9 +27,14 @@ class ApplicationTest extends TestCase
         $this->config = new ApplicationConfig(
             basePath: __DIR__,
             filePath: __FILE__,
-            baseUrl: "http://localhost:8000"
         );
     }
+
+    protected function tearDown(): void
+    {
+        Mockery::close();
+    }
+
 
     public function test_it_should_return_the_application()
     {
@@ -63,6 +72,27 @@ class ApplicationTest extends TestCase
         $app = Application::configure($this->config)->enableViews($viewsConfig)->create();
         $this->assertInstanceOf(Application::class, $app);
         $this->assertEquals($viewsConfig, $app->get(ViewConfig::class));
+    }
+
+    public function test_it_should_enable_vite_module()
+    {
+        $vite = Mockery::mock(Vite::class);
+        $vite->shouldReceive("handle")->once();
+
+        $app = new Application($this->config);
+        $app->set(Vite::class, $vite);
+
+        $vite = new ViteConfig(
+            publicPath: "/public",
+            entrypoints: ["resources/js/main.ts", "resources/css/main.css"],
+            url: "https://example.com/public"
+        );
+        
+        $appBuilder = new ApplicationBuilder($app);
+        $appBuilder->enableVite($vite);
+
+        $this->assertInstanceOf(Application::class, $app);
+        $this->assertEquals($vite, $app->get(ViteConfig::class));
     }
 
 
